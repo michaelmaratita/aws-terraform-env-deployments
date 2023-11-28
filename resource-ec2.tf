931b929d5c325b95bccec3db6b80e0567f05a420
Michael Maratita <mikool671@gmail.com>
2023-11-28 10:40:49 -0800

    #5 Successful test with utilizing python to create tf file for ec2 instances

--- resource-ec2.tf
+++ resource-ec2.tf
@@ -1,22 +1,43 @@
-resource "aws_instance" "dev-app1" {
-    ami = "ami-06e4ca05d431835e9"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.development_subnet.id
-    key_name = "test_linux"
-    root_block_device {
-      delete_on_termination = true
-      encrypted = true
-      volume_size = 10
+resource "aws_instance" "test_linux" {
+	ami = "ami-06e4ca05d431835e9"
+	instance_type = "t2.micro"
+	subnet_id = aws_subnet.development_subnet.id
+	key_name = "test_linux"
+	root_block_device {
+		delete_on_termination = true
+		encrypted = true
+		volume_size = 10
 
-      tags = {
-        Name = "dev-app1"
-        Environment = "Development"
-      }
-    }
+		tags = {
+			Name = "test_linux"
+			Environment = "{test-app-linux, root_vol}"
+		}
+	}
 
-    tags = {
-      Name = "Dev-App-Linux"
-      Environment = "Development"
-    }
-  
+	tags = {
+		Name = "test_linux"
+		Environment = "Development"
+	}
+}
+
+resource "aws_instance" "test_2" {
+	ami = "ami-06e4ca05d431835e9"
+	instance_type = "c5.xlarge"
+	subnet_id = aws_subnet.development_subnet.id
+	key_name = "test_linux"
+	root_block_device {
+		delete_on_termination = true
+		encrypted = true
+		volume_size = 10
+
+		tags = {
+			Name = "test_2"
+			Environment = "dev-app-linux"
+		}
+	}
+
+	tags = {
+		Name = "test_2"
+		Environment = "Development"
+	}
 }

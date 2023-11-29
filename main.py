151ca16ea15d83aa19d1e620c51bbe032a4c5745
Michael Maratita <mikool671@gmail.com>
2023-11-28 21:00:06 -0800

    #7 successfully refactored functions.py and resource-vpc files to eliminate static definitions.

--- main.py
+++ main.py
@@ -2,4 +2,3 @@ from functions import *
 
 ec2_vals = (read_csv('.\\test_py.csv'))
 tf_resource(ec2_vals)
-
---
+++ variables.tf
@@ -0,0 +1,63 @@
+variable "linux_ami" {
+  type = string
+}
+variable "windows_ami" {
+  type = string
+}
+variable "dev_cidr" {
+  type = string
+}
+variable "dev_app_subnet1" {
+  type = string
+}
+variable "dev_app_subnet2" {
+  type = string
+}
+variable "dev_db_subnet1" {
+  type = string
+}
+variable "dev_db_subnet2" {
+  type = string
+}
+variable "test_cidr" {
+  type = string
+}
+variable "test_app_subnet1" {
+  type = string
+}
+variable "test_app_subnet2" {
+  type = string
+}
+variable "test_db_subnet1" {
+  type = string
+}
+variable "test_db_subnet2" {
+  type = string
+}
+variable "preprod_cidr" {
+  type = string
+}
+variable "preprod_app_subnet1" {
+  type = string
+}
+variable "preprod_app_subnet2" {
+  type = string
+}
+variable "preprod_db_subnet1" {
+  type = string
+}
+variable "preprod_db_subnet2" {
+  type = string
+}
+variable "az1" {
+  type = string
+}
+variable "az2" {
+  type = string
+}
+variable "linux_key" {
+  type = string
+}
+variable "windows_key" {
+  type = string
+}

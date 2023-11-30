3f59bee21801e281e1a2835cb55260f4898c1783
Michael Maratita <118246054+tikit-tm@users.noreply.github.com>
2023-11-30 08:10:02 -0800

    #9 Added creation of Public Route Tables and IGW (#10)

--- resource-vpc.tf
+++ resource-vpc.tf
@@ -84,7 +84,7 @@ resource "aws_subnet" "test_db_subnet2" {
         Name = "Test-Subnet-4"
     }
 }
-resource "aws_vpc" "pre_prod" {
+resource "aws_vpc" "preprod" {
     cidr_block = var.preprod_cidr
 
     tags = {
@@ -92,7 +92,7 @@ resource "aws_vpc" "pre_prod" {
     }
 }
 resource "aws_subnet" "preprod_app_subnet1" {
-    vpc_id = aws_vpc.pre_prod.id
+    vpc_id = aws_vpc.preprod.id
     cidr_block = var.preprod_app_subnet1
     availability_zone = var.az1
 
@@ -101,7 +101,7 @@ resource "aws_subnet" "preprod_app_subnet1" {
     }
 }
 resource "aws_subnet" "preprod_app_subnet2" {
-    vpc_id = aws_vpc.pre_prod.id
+    vpc_id = aws_vpc.preprod.id
     cidr_block = var.preprod_app_subnet2
     availability_zone = var.az2
 
@@ -110,7 +110,7 @@ resource "aws_subnet" "preprod_app_subnet2" {
     }
 }
 resource "aws_subnet" "preprod_db_subnet1" {
-    vpc_id = aws_vpc.pre_prod.id
+    vpc_id = aws_vpc.preprod.id
     cidr_block = var.preprod_db_subnet1
     availability_zone = var.az1
 
@@ -119,11 +119,143 @@ resource "aws_subnet" "preprod_db_subnet1" {
     }
 }
 resource "aws_subnet" "preprod_db_subnet2" {
-    vpc_id = aws_vpc.pre_prod.id
+    vpc_id = aws_vpc.preprod.id
     cidr_block = var.preprod_db_subnet2
     availability_zone = var.az2
 
     tags = {
         Name = "Pre-Production-Subnet4"
     }
+}
+resource "aws_internet_gateway" "development" {
+  vpc_id = aws_vpc.development.id
+
+  tags = {
+    Name = "Development-IGW"
+  }
+}
+resource "aws_internet_gateway" "test" {
+  vpc_id = aws_vpc.test.id
+
+  tags = {
+    Name = "Test-IGW"
+  }
+}
+resource "aws_internet_gateway" "preprod" {
+  vpc_id = aws_vpc.preprod.id
+
+  tags = {
+    Name = "PreProd-IGW"
+  }
+}
+resource "aws_route_table" "Development-Public-Rt" {
+  vpc_id = aws_vpc.development.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.development.id
+  }
+
+  tags = {
+    Name = "Development-Public-rt"
+  }
+}
+resource "aws_route_table" "Test-Public-Rt" {
+  vpc_id = aws_vpc.test.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.test.id
+  }
+
+  tags = {
+    Name = "Test-Public-rt"
+  }
+}
+resource "aws_route_table" "PreProd-Public-Rt" {
+  vpc_id = aws_vpc.preprod.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.preprod.id
+  }
+
+  tags = {
+    Name = "PreProd-Public-rt"
+  }
+}
+resource "aws_default_route_table" "Development" {
+  default_route_table_id = aws_vpc.development.default_route_table_id
+
+  route = []
+
+  tags = {
+    Name = "Development-Private-Rt"
+  }
+}
+resource "aws_default_route_table" "Test" {
+  default_route_table_id = aws_vpc.test.default_route_table_id
+
+  route = []
+
+  tags = {
+    Name = "Test-Private-Rt"
+  }
+}
+resource "aws_default_route_table" "PreProd" {
+  default_route_table_id = aws_vpc.preprod.default_route_table_id
+
+  route = []
+
+  tags = {
+    Name = "PreProd-Private-Rt"
+  }
+}
+resource "aws_route_table_association" "Development-Public-1" {
+  subnet_id      = aws_subnet.dev_app_subnet1.id
+  route_table_id = aws_route_table.Development-Public-Rt.id
+}
+resource "aws_route_table_association" "Development-Public-2" {
+  subnet_id      = aws_subnet.dev_app_subnet2.id
+  route_table_id = aws_route_table.Development-Public-Rt.id
+}
+resource "aws_route_table_association" "Development-Private-1" {
+  subnet_id      = aws_subnet.dev_db_subnet1.id
+  route_table_id = aws_vpc.development.default_route_table_id
+}
+resource "aws_route_table_association" "Development-Private-2" {
+  subnet_id      = aws_subnet.dev_db_subnet2.id
+  route_table_id = aws_vpc.development.default_route_table_id
+}
+resource "aws_route_table_association" "Test-Public-1" {
+  subnet_id      = aws_subnet.test_app_subnet1.id
+  route_table_id = aws_route_table.Test-Public-Rt.id
+}
+resource "aws_route_table_association" "Test-Public-2" {
+  subnet_id      = aws_subnet.test_app_subnet2.id
+  route_table_id = aws_route_table.Test-Public-Rt.id
+}
+resource "aws_route_table_association" "Test-Private-1" {
+  subnet_id      = aws_subnet.test_db_subnet1.id
+  route_table_id = aws_vpc.test.default_route_table_id
+}
+resource "aws_route_table_association" "Test-Private-2" {
+  subnet_id      = aws_subnet.test_db_subnet2.id
+  route_table_id = aws_vpc.test.default_route_table_id
+}
+resource "aws_route_table_association" "PreProd-Public-1" {
+  subnet_id      = aws_subnet.preprod_app_subnet1.id
+  route_table_id = aws_route_table.PreProd-Public-Rt.id
+}
+resource "aws_route_table_association" "PreProd-Public-2" {
+  subnet_id      = aws_subnet.preprod_app_subnet2.id
+  route_table_id = aws_route_table.PreProd-Public-Rt.id
+}
+resource "aws_route_table_association" "PreProd-Private-1" {
+  subnet_id      = aws_subnet.preprod_db_subnet1.id
+  route_table_id = aws_vpc.preprod.default_route_table_id
+}
+resource "aws_route_table_association" "PreProd-Private-2" {
+  subnet_id      = aws_subnet.preprod_db_subnet2.id
+  route_table_id = aws_vpc.preprod.default_route_table_id
 }
